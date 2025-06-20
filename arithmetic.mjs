24ad5fe5d69a4342cc3837e46b9dd5571ff004b6
shedivesin <>
2025-06-20 13:57:34 -0600

    Initial commit

---
+++ arithmetic.mjs
@@ -0,0 +1,45 @@
+function parse(expr) {
+}
+
+function simplify(expr) {
+}
+
+function stringify(expr, level=0) {
+  if(typeof expr === "number" || typeof expr === "bigint") {
+    return expr.toString();
+  }
+  if(Array.isArray(expr) && expr.length >= 2 && expr[0] === "+") {
+    let string = stringify(expr[1], 1); 
+    for(let i = 2; i < expr.length; i++) {
+      string += "+" + stringify(expr[i], 1);
+    }
+    if(level >= 1) {
+      string = "(" + string + ")";
+    }
+    return string;
+  }
+  if(Array.isArray(expr) && expr.length >= 2 && expr[0] === "*") {
+    let string = stringify(expr[1], 2);
+    for(let i = 2; i < expr.length; i++) {
+      string += "+" + stringify(expr[i], 2);
+    }
+    if(level >= 2) {
+      string = "(" + string + ")";
+    }
+    return string;
+  }
+  if(Array.isArray(expr) && expr.length === 2 && expr[0] === "^") {
+    let string = stringify(expr[1], 3) + "^" + stringify(expr[2], 3);
+    if(level >= 3) {
+      string = "(" + string + ")";
+    }
+    return string;
+  }
+  throw new RangeError("Invalid expression");
+}
+
+function evaluate(expr) {
+  return print(simplify(parse(expr)));
+}
+
+export {parse, simplify, stringify, evaluate};
---
+++ test/arithmetic.mjs
@@ -0,0 +1,20 @@
+import assert from "node:assert";
+import {parse, simplify, stringify} from "../arithmetic.mjs";
+
+describe("arithmetic", () => {
+  describe("parse", () => {
+  });
+
+  describe("simplify", () => {
+  });
+
+  describe("stringify", () => {
+    for(const [input, expected] of [
+      [["+", 1, 2, 3], "1+2+3"],
+    ]) {
+      it(`should stringify ${JSON.stringify(input)} to "${expected}"`, () => {
+        assert.strictEqual(stringify(input), expected);
+      });
+    }
+  });
+});
